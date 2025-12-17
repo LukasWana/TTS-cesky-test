8bb0584ba483e0785debdd8827dde7f82ec474f9
Lukas <qwanap@gmail.com>
2025-12-17 20:48:02 +0100

    Smazán __start_gpu.bat a odstraněny chybné soubory (nul, Node.js, Okno, Python, Windows)

--- __start_gpu.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Spustí aplikaci s vynuceným GPU device
-echo ========================================
-echo Spouštím aplikaci s GPU device
-echo ========================================
-echo.
-set FORCE_DEVICE=cuda
-call start_all.bat
-
