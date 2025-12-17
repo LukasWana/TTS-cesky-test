d217489ab31ecc9b69b71a4d6197db16766f7b8c
Lukas <qwanap@gmail.com>
2025-12-17 17:30:14 +0100

    feat: přidání Tabs komponenty pro přepínání mezi sekcemi

---
+++ frontend/src/components/Tabs.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Tabs.css'
+
+function Tabs({ activeTab, onTabChange, tabs }) {
+  return (
+    <div className="tabs-container">
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          className={`tab-button ${activeTab === tab.id ? 'active' : ''}`}
+          onClick={() => onTabChange(tab.id)}
+        >
+          {tab.icon && <span className="tab-icon">{tab.icon}</span>}
+          <span className="tab-label">{tab.label}</span>
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default Tabs
+
